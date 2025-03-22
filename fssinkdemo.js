f8cc73ce0965292c45e04e66b415ca1b2534e844
parikshit panchal <parikshitpanchal016@gmail.com>
2025-03-22 10:36:21 +0530

    Add files via upload

---
+++ fssinkdemo.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+const fs=require('fs');
+
+// 1.Create folder
+// fs.mkdirSync("test");
+// fs.mkdirSync("./abc/test1",{recursive:true});
+
+// 2.Write File(Create)
+//fs.writeFileSync("./test/a1.txt","Some text return in file.");
+
+// 3.Append File(Create)
+// console.log("File writen");
+// fs.appendFileSync("./test/a1.txt","Hello World");
+// console.log("File appended");
+
+// 4.Read File
+// var data=fs.readFileSync("./test/a1.txt"); or UTF-8
+// console.log(data);
+// console.log(data.toString());
+
+
+// 6.Rename File
+//fs.renameSync("./test/a1.txt","./test/a2.txt");
+
+// 5.Delete File
+//fs.unlinkSync("./test/a2.txt");
+
+// 7.Delete Folder
+// fs.rmdirSync("./test"); 
