7f70f5cfb620fd098afda2e471c9c7cb10fa0ae7
parikshit panchal <parikshitpanchal016@gmail.com>
2025-03-25 12:44:29 +0530

    Add files via upload

---
+++ p6.js
@@ -0,0 +1,16 @@
+// write a script to write no. in file a.txt seprated by space.
+// read this file sort number in ascending number and write in b.txt.
+
+const fs=require('fs');
+//fs.mkdirSync("test1");
+
+
+
+//fs.writeFileSync("./test1/a.txt","4 2 6 1 3 7 9");
+
+var data=fs.readFileSync("./test1/a.txt","UTF-8");
+
+var ar=data.split(" ");
+ar.sort();
+
+fs.writeFileSync("./test1/b.txt",ar.toString());
