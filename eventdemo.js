8cbfcac329115cfa3ad10bfbdc0c7ece524eab85
parikshit panchal <parikshitpanchal016@gmail.com>
2025-04-23 18:54:36 +0530

    Delete eventdemo.js

--- eventdemo.js
+++
@@ -1,8 +0,0 @@
-const ee=require('events');
-const e=new ee();
-e.on('print',(m)=>console.log("Hello world "+m));
-e.on('add',(a,b)=>console.log("Hello world "+(a+b)));
-e.on('sub',(a,b)=>console.log("Hello world "+(a-b)));
-e.emit('print','from Fp');
-e.emit('add',30,40);
-e.emit('sub',30,40);
